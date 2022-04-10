b78e06016d49f49e37e94b3a2e3a42184a68615c
Surf <1398470095@qq.com>
2022-04-10 10:53:35 +0800

    push commit

--- src/main/java/com/Surf/git.java
+++ src/main/java/com/Surf/git.java
@@ -9,6 +9,8 @@ public class git {
         System.out.println("git2");
         System.out.println("git3");
         System.out.println("git4");
+        System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push");
     }
 }
